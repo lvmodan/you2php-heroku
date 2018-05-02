1532595960ed006a9351dcde7e6cf5f62b655db7
fire <AmyGitHub@126.com>
2018-05-02 04:57:17 -0700

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBT43n8loSUhq9vW5baVhuSdhLmRGIh7mg');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('GJ_CODE', 'HK');//国家代码
+define('SITE_NAME', 'sitename');//网站名字
+define('TITLENAME', 'titlename');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh3');//一个随机字符串，加解密KEY
+define('EMAIL', 'modan518@gmail.com');//邮箱
 ?>
