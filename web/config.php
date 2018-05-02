3d329750544919ff003362d4b6f8010a9c3ddee6
fire <AmyGitHub@126.com>
2018-05-02 05:26:30 -0700

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBT43n8loSUhq9vW5baVhuSdhLmRGIh7mg');//这里改成您的api key
+define('APIKEY', 'AIzaSyDxYfKW6_gzgxvxqAo0QshLrovU5R2I8HU');//这里改成您的api key
 define('GJ_CODE', 'UK');//国家代码
 define('SITE_NAME', 'sitename');//网站名字
 define('TITLENAME', 'titlename');//网站名字
